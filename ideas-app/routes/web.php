37118ed9e0f264bf8754f09219f5b0974f40d359
Claudiu H <claudiu.hasegan@gmail.com>
2024-04-29 21:45:59 +0300

    Created controllers from terminal & deleted some stuff

--- ideas-app/routes/web.php
+++ ideas-app/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,14 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// when you access '/' route, use 'index' method from 'DashboardController'
+Route::get('/', [DashboardController::class, 'index']);
 
-Route::get('/feed', function () {
-    return view('feed');
-});
-
-Route::get('/profile', function () {
-    return view('users.profile');
-});
+Route::get('/profile', [ProfileController::class, 'index']);
