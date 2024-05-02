90c06ea55110c3e12076b01b181283ee063ca485
Claudiu H <claudiu.hasegan@gmail.com>
2024-05-02 23:35:10 +0300

    Created db connection, added 1 migration & 1 model for the 'idea', added and displayed the 'idea'

--- ideas-app/app/Http/Controllers/DashboardController.php
+++ ideas-app/app/Http/Controllers/DashboardController.php
@@ -2,26 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Idea;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $users = [
-            [
-                'name' => 'Alex',
-                'age' => 30,
-            ],
-            [
-                'name' => 'Dan',
-                'age' => 25,
-            ],
-            [
-                'name' => 'John',
-                'age' => 17,
-            ],
-        ];
-        return view('dashboard', ['users' => $users]);
+        // $idea = new Idea([
+        //     'content' => 'hello yt',
+        // ]);
+        // $idea->save();
+
+        return view('dashboard', [
+            'ideas' => Idea::orderBy('created_at', 'DESC')->get()
+        ]);
     }
 }
