95b07c687847967c72261285bc7c2144f10aaeac
Claudiu H <claudiu.hasegan@gmail.com>
2024-05-03 14:31:31 +0300

    Show, update & delete an 'idea'

--- ideas-app/resources/views/shared/idea-card.blade.php
+++ ideas-app/resources/views/shared/idea-card.blade.php
@@ -9,11 +9,38 @@
                         </a></h5>
                 </div>
             </div>
-            <div>
+            <div class="d-flex flex-row">
+                {{-- SHOW --}}
+                <a href="{{ route('ideas.show', $idea->id) }}"
+                    class="link-light link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">
+                    <button class="btn btn-secondary btn-sm" style="background-color: #0d6efd; border-color:#0d6efd;">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-eye-fill" viewBox="0 0 16 16">
+                            <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0" />
+                            <path
+                                d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7" />
+                        </svg>
+
+                    </button>
+                </a>
+
+                {{-- UPDATE --}}
+                <a href="{{ route('ideas.edit', $idea->id) }}"
+                    class="ms-2 link-light link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">
+                    <button class="btn btn-secondary btn-sm" style="background-color: #198754; border-color: #198754;">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                            <path
+                                d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z" />
+                        </svg>
+                    </button>
+                </a>
+
+                {{-- DELETE --}}
                 <form action="{{ route('ideas.destroy', $idea->id) }}" method="post">
                     @csrf
                     @method('delete')
-                    <button class="btn btn-danger btn-sm">
+                    <button class="ms-2 btn btn-danger btn-sm">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                             class="bi bi-trash3-fill" viewBox="0 0 16 16">
                             <path
@@ -26,9 +53,27 @@
     </div>
 
     <div class="card-body">
-        <p class="fs-6 fw-light text-muted">
-            {{ $idea->content }}
-        </p>
+        @if ($editing ?? false)
+            <form action="{{ route('ideas.update', $idea->id) }}" method="post">
+                @csrf
+                @method('put')
+                <div class="mb-3">
+                    <textarea name="content" class="form-control" id="content" rows="3">{{ $idea->content }}</textarea>
+                    @error('content')
+                        {{-- laravel directly inject the error message --}}
+                        <span class="d-block fs-6 text-danger mt-2">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div class="">
+                    <button type="submit" class="btn btn-dark"> Share </button>
+                </div>
+            </form>
+        @else
+            <p class="fs-6 fw-light text-muted">
+                {{ $idea->content }}
+            </p>
+        @endif
+
         <div class="d-flex justify-content-between">
             <div>
                 <a href="#" class="fw-light nav-link fs-6"> <span class="fas fa-heart me-1">
