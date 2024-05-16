c6db6ca79582351bb90d2c58cd476a59d3a17bc3
Claudiu H <claudiu.hasegan@gmail.com>
2024-05-16 14:44:27 +0300

    Added admin dashboard for admin users (middleware basics)

--- ideas-app/routes/web.php
+++ ideas-app/routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\IdeaLikeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -47,3 +48,6 @@ Route::get('/feed', FeedController::class)->middleware('auth')->name('feed');
 Route::get('/terms', function () {
     return view('terms');
 })->name('terms');
+
+
+Route::get('/admin', [AdminDashboardController::class, 'index'])->middleware(['auth', 'admin'])->name('admin.dashboard');
